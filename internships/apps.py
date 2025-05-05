b2363b289b2638d40cf56f18fb0d654a75b61216
zsmithc2c <z.t.smith1696@gmail.com>
2025-05-05 12:03:24 -0500

    chore: align isort with black; add OpenAPI stub

--- internships/apps.py
+++ internships/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class InternshipsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'internships'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "internships"
